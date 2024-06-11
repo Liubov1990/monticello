ae52e4da9ed441776560e0e23bf28375c7953302
Liuba <1990yavorska@gmail.com>
2024-06-11 14:42:04 +0200

    Added 'news' component

--- src/js/constants/slider.js
+++ src/js/constants/slider.js
@@ -13,3 +13,40 @@ export const heroSliderOptions = {
     clickable: true
   }
 };
+
+export const newsSliderOptions = {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.arrow--next',
+    prevEl: '.arrow--prev'
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+};
